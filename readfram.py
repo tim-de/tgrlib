baadf937a811643eeb90212926fa99b9618676e4
Tim Evans <evanstde@gmail.com>
2023-03-16 20:52:10 +0000

    All portraits now read, and work started on sprites
    The portraits section can now be entirely read (barring
    the icons, which seem to use a similar encoding to sprites)
    and the line headers for sprites can be read.
    
    Pixels are now internally handled as a pixel dataclass,
    rather than being passed around in several different
    nonstandard formats.

--- readfram.py
+++ readfram.py
@@ -1,62 +1,9 @@
 #!/usr/bin/pypy3
 
 import sys
-import io
 import tgrlib
 from PIL import Image
 from pathlib import Path
-import struct
-
-def getRunData(byte):
-    flag = byte >> 5
-    length = byte & 31
-    return (flag, length)
-
-def decodePixel(half_word: int):
-    # Shift values move each colour channel to the right
-    # place value.
-    # Bitwise AND masks off the unwanted bits to leave
-    # only the desired channel.
-    blue = (half_word << 3) & 0xff
-    green = (half_word >> 3) & 0xfc
-    red = (half_word >> 8) & 0xf8
-    return struct.pack("BBB", red, green, blue)
-
-def extractLine(line: tgrlib.tgrFile.frame.line, fh: io.BufferedReader, line_idx=None):
-    outbuf = b""
-    line_ix = 0
-    pixel_ix = 0
-    fh.seek(line.offset)
-    #print(f"Extracting line of length 0x{line.length:x}")
-    while line_ix < line.data_length:
-        run_header = fh.read(1)
-        line_ix += 1
-        (flag, run_length) = getRunData(run_header[0])
-        if flag == 1:
-            (raw_pixel,) = struct.unpack("H", fh.read(2))
-            pixel = decodePixel(raw_pixel)
-            outbuf += pixel * run_length
-            pixel_ix += run_length
-            line_ix += 2
-        elif flag == 2:
-            for _ in range(run_length):
-                (raw_pixel,) = struct.unpack("H", fh.read(2))
-                outbuf += decodePixel(raw_pixel)
-                line_ix += 2
-                pixel_ix += 1
-        elif flag == 6:
-            outbuf += struct.pack("BBB", 8*run_length, 0, 0)
-            line_ix += 1
-            pixel_ix += 1
-        elif flag == 7:
-            for _ in range(run_length):
-                outbuf += struct.pack("BBB", 8, 0, 0)
-                pixel_ix += 1
-            fh.seek((run_length + 1) // 2, 1)
-            line_ix += ((run_length + 1) // 2) + 1
-        else:
-            print(f"{line_idx:3d},{pixel_ix:3d}: Unsupported flag {flag} in datapoint 0x{run_header[0]:02x} at offset 0x{fh.tell()-1:08x}")
-    return outbuf     
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -64,41 +11,36 @@ if __name__ == "__main__":
         exit()
     image_path = sys.argv[1]
 
-    imagefile = tgrlib.tgrFile(image_path)
+    imagefile = tgrlib.tgrFile(image_path, False)
 
     imagefile.load()
 
     image_name = Path(image_path).stem
 
+    #Path(image_name).mkdir(exist_ok=True)
+
+    frame_index = 0
+    pixel_format = "RGB"
+
+    for frame_index, frame in enumerate([imagefile.frames[0]]):
+
     #print(imagefile.framecount)
-    frame = imagefile.frames[0]
-    #outfilename = "out.bin"
+    # frame = imagefile.frames[frame_index]
 
-    #min = frame.size[0]
-    #min_idx = 0
-    #for index, line in enumerate(frame.lines):
-    #    if line.length < min:
-    #        min = line.length
-    #        min_idx = index
+        print(frame.size)
+        image = Image.new(pixel_format, frame.size)
+        imagedata = b""
+        with open(image_path, "rb") as in_fh:
+            for idx in range(len(frame.lines)):
+                rawline = imagefile.extractLine(in_fh, frame_index=frame_index, line_index=idx, increment=0)
+                #print(f"{idx+1:3d}: 0x{frame.lines[idx].offset:06x}, {len(rawline)}")
+                while len(rawline) < frame.size[0]:
+                    rawline.append(tgrlib.Pixel(0, 0, 0))
+                if len(rawline) > frame.size[0]:
+                    rawline = rawline[0:frame.size[0]]
+                imagedata += b"".join([elem.pack_to_bin(pixel_format) for elem in rawline])
+                #print(len(imagedata))
+        image.frombytes(imagedata)
+        # image.save(f"{image_name}/fram_{frame_index}.png")
 
-    #shortline = frame.lines[min_idx]
-    #print(f"{min_idx}: 0x{shortline.offset:06x}, {shortline.length}")
-
-    #with open(outfilename, "wb") as out_fh:
-    #    with open(imagefile.filename, "rb") as in_fh:
-    #        in_fh.seek(shortline.offset)
-    #        buf = in_fh.read(shortline.length)
-    #        out_fh.write(buf)
-    
-    image = Image.new("RGB", frame.size)
-    imagedata = b""
-    with open(image_path, "rb") as in_fh:
-        for idx, line in enumerate(frame.lines):
-            rawline = extractLine(line, in_fh, idx)
-            imagedata += rawline
-            if len(rawline) < (3 * frame.size[0]):
-                imagedata += b"\xff" * ((3*line.pixel_length) - len(rawline))
-            #print(f"{idx+1:3d}: 0x{line.offset:06x}, {len(rawline)}")
-    #print(len(imagedata))
-    image.frombytes(imagedata)
-    image.save(f"{image_name}.png")
+        image.save(f"{image_name}.png")
--- tgrlib.py
+++ tgrlib.py
@@ -3,11 +3,12 @@
 import ifflib
 import struct
 import io
+from dataclasses import dataclass
 
 def read_line_length(in_fh: io.BufferedReader):
     rawlen = in_fh.read(2)
     if len(rawlen) < 2:
-        raise EOFError
+        return 0
     if rawlen[0] & 0x80 != 0:
         (length,) = struct.unpack(">H", rawlen)
         return length & 0x7fff
@@ -15,45 +16,117 @@ def read_line_length(in_fh: io.BufferedReader):
         in_fh.seek(-1, 1)
         return rawlen[0]
 
+def get_sprite_line_info(in_fh: io.BufferedReader):
+    data = in_fh.read(3)
+    if len(data) < 3:
+        return {"data_length": 0}
+    return {"data_length": data[0],
+            "start": data[1],
+            "pixel_length": data[2]}
+
+def getRunData(byte):
+    flag = byte >> 5
+    length = byte & 31
+    return (flag, length)
+
+@dataclass
+class Pixel:
+    """Class for managing pixel values in different formats"""
+    red: int
+    green: int
+    blue: int
+    alpha: int = 0xff
+
+    @classmethod
+    def from_int(cls, half_word: int):
+        # Shift values move each colour channel to the right
+        # place value.
+        # Bitwise AND masks off the unwanted bits to leave
+        # only the desired channel.
+        blue = (half_word << 3) & 0xff
+        green = (half_word >> 3) & 0xfc
+        red = (half_word >> 8) & 0xf8
+        return cls(red, green, blue)
+
+    def pack_to_bin(self, format: str ="RGB"):
+        if format == "RGB":
+            return struct.pack("BBB", self.red, self.green, self.blue)
+        elif format == "RGBA":
+            return struct.pack("BBBB", self.red, self.green, self.blue, self.alpha)
+        else:
+            raise Exception("Invalid pixel format specifier")
+
+def packPixel(value=(0,0,0), alpha=False):
+    if len(value) < 3:
+        raise ValueError("Not enough pixel data")
+    if alpha:
+        if len(value) == 3:
+            return struct.pack("BBBB", value[0], value[1], value[2], 0xff)
+        return struct.pack("BBBB", value[0], value[1], value[2], value[3])
+    return struct.pack("BBB", value[0], value[1], value[2])
+
+def decodePixel(half_word: int):
+    # Shift values move each colour channel to the right
+    # place value.
+    # Bitwise AND masks off the unwanted bits to leave
+    # only the desired channel.
+    blue = (half_word << 3) & 0xff
+    green = (half_word >> 3) & 0xfc
+    red = (half_word >> 8) & 0xf8
+    return Pixel(red, green, blue)
+
+class Line:
+    def __init__(self, in_fh: io.BufferedReader, sprite=False):
+        header_offset = in_fh.tell()
+        if sprite:
+            # print("Fetching sprite line data")
+            data = in_fh.read(3)
+            if len(data) < 3:
+                self.data_length = 0
+                self.transparent_pixels = 0
+                self.pixel_length = 0
+                return
+            else:
+                total_length = data[0]
+                self.transparent_pixels = data[1]
+                self.pixel_length = data[2]
+        else:
+            total_length = read_line_length(in_fh)
+            in_fh.seek(1,1)
+            self.pixel_length = read_line_length(in_fh)
+        self.offset = in_fh.tell()
+        self.data_length = total_length - (self.offset - header_offset)
+                
+        def get(self, in_fh: io.BufferedReader):
+            in_fh.seek(self.offset)
+            return in_fh.read(self.data_length)
+
+class Frame:
+    def __init__(self, size, in_fh: io.BufferedReader):
+        self.size = size
+        self.lines = []
+        while True:
+            newline = Line(in_fh, False)
+            if newline.data_length == 0:
+                break
+            self.lines.append(newline)
+            if len(self.lines) >= self.size[1]:
+                break
+            in_fh.seek(newline.offset + newline.data_length)
+
 class tgrFile:
     """
     A class representing a .TGR game asset file,
     which as a format is based on the IFF file structure
     """
-    
-    class frame:
-        class line:
-            def __init__(self, in_fh: io.BufferedReader):
-                header_offset = in_fh.tell()
-                total_length = read_line_length(in_fh)
-                in_fh.seek(1,1)
-                self.pixel_length = read_line_length(in_fh)
-                self.offset = in_fh.tell()
-                self.data_length = total_length - (self.offset - header_offset)
-            def get(self, in_fh: io.BufferedReader):
-                in_fh.seek(self.offset)
-                return in_fh.read(self.data_length)
-            #def get(self, filename: str):
-            #    with open(filename, "rb") as in_fh:
-            #        return self.get(in_fh)
-
-        def __init__(self, size, in_fh: io.BufferedReader):
-            self.size = size
-            self.lines = []
-            while True:
-                newline = tgrFile.frame.line(in_fh)
-                if newline.data_length == 0 or len(self.lines) >= self.size[1]:
-                    break
-                self.lines.append(newline)
-                in_fh.seek(newline.offset + newline.data_length)
-
         # TODO: add methods for decoding a frame into either a list of bytes
         #       or a PIL image (but might be best to keep it light on dependencies)
                 
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, is_sprite=False):
         self.filename = filename
         self.iff = ifflib.iff_file(self.filename)
         self.size = None
+        self.is_sprite = is_sprite
         self.framesizes = []
         self.frames = []
 
@@ -62,16 +135,26 @@ class tgrFile:
         if self.iff.data.formtype != "TGAR":
             print(f"Error: invalid file type: {self.iff.data.formtype}")
         self.read_header()
+        if self.indexed_colour:
+            self.load_palette()
         self.get_frames()
 
     def read_header(self):
         with open(self.filename, "rb") as in_fh:
             in_fh.seek(self.iff.data.children[0].data_offset)
-            in_fh.seek(4, 1)
-            (self.framecount,) = struct.unpack("H", in_fh.read(2))
-            in_fh.seek(6, 1)
+            (self.version,
+             self.framecount,
+             self.bits_per_px) = struct.unpack("IHBx", in_fh.read(8))
+            (index_mode,
+             self.offset_flag) = struct.unpack("xBBx", in_fh.read(4))
             self.size = struct.unpack("HH", in_fh.read(4))
-            in_fh.seek(24, 1)
+            self.hotspot = struct.unpack("HH", in_fh.read(4))
+            
+            #print(self.offset_flag)
+            self.indexed_colour = index_mode & 0x7f == 0x1a
+            in_fh.seek(20, 1)
+            #if self.indexed_colour:
+            #    in_fh.seek(12, 1)
             for _ in range(self.framecount):
                 (ulx, uly, lrx, lry, offset) = struct.unpack("HHHHI", in_fh.read(12))
                 #in_fh.seek(4, 1)
@@ -79,12 +162,78 @@ class tgrFile:
                 self.framesizes.append((1+lrx-ulx, 1+lry-uly, offset))
         #print(len(self.framesizes))
 
+    def load_palette(self):
+        palt = self.iff.data.children[1]
+        self.palette = []
+        with open(self.filename, "rb") as in_fh:
+            in_fh.seek(palt.data_offset)
+            (count,) = struct.unpack("<I", in_fh.read(4))
+            print(count)
+            for _ in range(count):
+                raw_pixel = in_fh.read(2)
+                if len(raw_pixel) < 2:
+                    raise ValueError("Not enough image data")
+                (pixel,) = struct.unpack("H", raw_pixel)
+                self.palette.append(Pixel.from_int(pixel))
+        #print(len(self.palette))
+    
     def get_frames(self):
         with open(self.filename, "rb") as in_fh:
             for child in self.framesizes:
                 in_fh.seek(child[2])
-                frame = tgrFile.frame((child[0], child[1]), in_fh)
-                self.frames.append(frame)
+                newframe = Frame((child[0], child[1]), in_fh)
+                self.frames.append(newframe)
 
+    def get_next_pixel(self, in_fh: io.BufferedReader):
+        if self.indexed_colour:
+            (pixel_ix,) = struct.unpack("B", in_fh.read(1))
+            return self.palette[pixel_ix-1]
+        else:
+            (raw_pixel,) = struct.unpack("H", in_fh.read(2))
+            return Pixel.from_int(raw_pixel)
+
+    def extractLine(self, fh: io.BufferedReader, frame_index=0, line_index=0, increment=0):
+        outbuf = []
+        line_ix = 0
+        pixel_ix = 0
+        line = self.frames[frame_index].lines[line_index]
+        fh.seek(line.offset)
+        # print(f"Extracting line of length 0x{line.pixel_length:x}")
+        if self.is_sprite:
+            # print(f"Writing {line.transparent_pixels} transparent pixels")
+            for _ in range(line.transparent_pixels):
+                outbuf.append(Pixel(0, 0, 0, 0))
+            pixel_ix += line.transparent_pixels
+        
+        while line_ix < line.data_length:# and pixel_ix < line.pixel_length:
+            run_header = fh.read(1)
+            line_ix += 1
+            (flag, run_length) = getRunData(run_header[0])
+            if flag == 1:
+                pixel = self.get_next_pixel(fh)
+                outbuf += [pixel for _ in range(run_length+increment)]
+                pixel_ix += run_length+increment
+                line_ix += self.bits_per_px // 8
+            elif flag == 2:
+                for _ in range(run_length+increment):
+                    outbuf.append(self.get_next_pixel(fh))
+                    line_ix += self.bits_per_px // 8
+                    pixel_ix += 1
+            elif flag == 6:
+                outbuf.append(Pixel(8*run_length, 0, 0))
+                line_ix += 1
+                pixel_ix += 1
+            elif flag == 7:
+                for _ in range(run_length+increment):
+                    outbuf.append(Pixel(8, 0, 0))
+                    pixel_ix += 1
+                fh.seek((run_length + 1) // 2, 1)
+                line_ix += ((run_length + 1) // 2) + 1
+            else:
+                    print(f"{line_index:3d},{pixel_ix:3d}: Unsupported flag {flag} in datapoint 0x{run_header[0]:02x} at offset 0x{fh.tell()-1:08x}")
+        #if len(outbuf) < 3*line.pixel_length:
+        #    outbuf += b"\xff" * ((3*line.pixel_length) - len(outbuf))
+        return outbuf
+            
 if __name__ == "__main__":
     pass
